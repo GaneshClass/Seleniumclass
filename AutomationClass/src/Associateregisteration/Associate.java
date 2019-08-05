bb65d1ca6d97a89110332b7c7b307b604c524430
Virendra <virendra.kumar@murcul.tech>
2019-08-05 14:13:36 +0530

    updated code

--- AutomationClass/src/Associateregisteration/Associate.java
+++ AutomationClass/src/Associateregisteration/Associate.java
@@ -1,7 +1,10 @@
 package Associateregisteration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Associate 
 {
@@ -23,12 +26,19 @@ public class Associate
 	Check_Zip.click();
 
 		
-	WebElement Apply_Button = driver.findElementByXPath("//*a[text()[contains(.,'Apply Now')]]");
+	WebElement Apply_Button = driver.findElement( By.linkText("Apply Now"));
 	Apply_Button.click();  
 	
 			
 	//WebElement Apply = driver.findElementByXPath("//*[text()[contains(.,'Apply Now')]]");
 	//Apply.click();
+	
+	
+	new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//label[@for='term_and_conditions']"))).click();
+	
+	
+	WebElement next = driver.findElementById("consentToProceed");
+	next.click();
 	
 	WebElement First_Name= driver.findElementById("Register_firstname");
 	First_Name.sendKeys("Test");
@@ -49,10 +59,10 @@ public class Associate
 	WebElement Email_id= driver.findElementById("Register_email");
 	Email_id.sendKeys("testa@mailinator.com");
 	
-    WebElement checkbox = driver.findElementByXPath("//*[text()[contains(.,' Consent to Contact')]]");
+    WebElement checkbox1 = driver.findElementByXPath("//*[text()[contains(.,' Consent to Contact')]]");
 	
 	//WebElement checkbox = driver.findElementByXPath("/html/body/div[1]/div[1]/form/section/div/div[2]/div[6]/div/div/div/label");
-    checkbox.click();
+    checkbox1.click();
     
     WebElement register= driver.findElementById("registerEmail");
 	register.click();
